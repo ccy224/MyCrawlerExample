dbcf58775df0440468e2b359cc72cc9ffa585194
ccy224 <ccy224@icloud.com>
2017-12-10 09:57:31 +0800

    test

--- src/crawler/example/exam/FacebookExam.java
+++ src/crawler/example/exam/FacebookExam.java
@@ -25,8 +25,8 @@ public class FacebookExam {
 
 		String uri = 
 				"https://graph.facebook.com/v2.6"
-				+ "/search?q="
-				+ "&access_token=";
+				+ "/judgead/posts?fields=id,link,message,created_time,likes.limit(0).summary(total_count),reactions.limit(0).summary(total_count)"
+				+ "&access_token=EAACEdEose0cBAHaLXqlnIQlaCbDbC7keRUaw6a8ZAH7dp15UAMnrkURUfe8QIAuq14th5YpPCH8IZAJYVpZAhXGnmYZBu5s5f3uPrDPLykMZCKRziKe57OhPIxcTzXqg6IbDsw0vuh3lGrFBKrQNGfZAwjnRSZC6iRNrCJhJJP85mmMVfv0xYnYH8TZClLZAZAReUZD";
 
 
 		Elements elems =
@@ -41,7 +41,7 @@ public class FacebookExam {
 			String id = data.select("id").text();
 
 			// FIXIT
-			String reactions = "";
+			String reactions = data.select("reactions summary total_count ").text();
 
 
 			output += id + "," + reactions + "\n";
